bc8e1c942620ec129a994b6d520d70cb10c8a483
putra4648 <wakwaujaim@gmail.com>
2024-03-09 22:35:05 +0700

    added primeng and primeflex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'archana-berry-website';
