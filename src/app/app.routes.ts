1ae4e32b683ad1ffd2c9ffc8f677d2754d484ce0
putra4648 <wakwaujaim@gmail.com>
2024-03-10 23:20:23 +0700

    added new layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { InformationComponent } from './information/information.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "technology",
+    component: TechnologyComponent
+  },
+  {
+    path: "information",
+    component: InformationComponent
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
---
+++ src/app/information/information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './information.component.html',
+  styles: ``
+})
+export class InformationComponent {
+
+}
